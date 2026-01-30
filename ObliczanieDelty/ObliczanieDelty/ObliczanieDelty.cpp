03af6a9e62acd20ad87d0d121a747d4c3e7874cd
Sebian12 <256605829+Sebian12@users.noreply.github.com>
2026-01-30 20:40:35 +0100

    Ukończono prosty licznik miejsc zerowych w funkcji kwadratowej

--- ObliczanieDelty/ObliczanieDelty/ObliczanieDelty.cpp
+++ ObliczanieDelty/ObliczanieDelty/ObliczanieDelty.cpp
@@ -4,6 +4,26 @@ using namespace std;
 
 int main()
 {
-	cout << "Program do obliczania delty dla rownania kwadratowego ax^2 + bx + c = 0" << endl;
-}
+	// Program do obliczania delty dla rownania kwadratowego ax^2 + bx + c = 0
+	double a, b, c;
 
+	cout << "Podaj wspolczynnik a: ";
+	cin >> a;
+	cout << "Podaj wspolczynnik b: ";
+	cin >> b;
+	cout << "Podaj wspolczynnik c: ";
+	cin >> c;
+
+	double delta = (b * b) - (4 * a * c);
+
+	double x1, x2;
+
+	double pierwiastek_delta = sqrt(delta);
+
+	x1 = (-b - pierwiastek_delta) / (2 * a);
+	x2 = (-b + pierwiastek_delta) / (2 * a);
+
+	cout << "Pierwsze miejsce zerowe: " << x1 << endl;
+	cout << "Drugie miejsce zerowe: " << x2 << endl;
+
+}
